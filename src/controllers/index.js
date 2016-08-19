be04f1fd5c9d07772ebd0612bc8950f0efa6d895
Andrey Zubkov <oxymoron@zubkov-mac.local>
2016-08-18 23:37:55 -0700

    Checking if authorization missing here...

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -29,6 +29,18 @@ router.post('/webhook', function (req, res) {
     res.sendStatus(200);
 });
 
+router.get('/authorize', function(req, res) {
+    var accountLinkingToken = req.query['account_linking_token'];
+    var redirectURI = req.query['redirect_uri'];
+    var authCode = "1234567890";
+    var redirectURISuccess = redirectURI + "&authorization_code=" + authCode;
+    res.render('authorize', {
+        accountLinkingToken: accountLinkingToken,
+        redirectURI: redirectURI,
+        redirectURISuccess: redirectURISuccess
+    });
+});
+
 
 
 
